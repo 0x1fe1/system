af3096f08bfb9f9d090da72e0f62567467bb7965
Pangolecimal <domkuzaleza@gmail.com>
2024-02-28 22:12:13 +0300

    default: 73 current  2024-02-28 22:09:00  24.05.20240216.5863c27  6.1.77                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -20,8 +20,6 @@
   system.stateVersion = "23.11";
 
   # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
   boot = {
     loader = {
       # systemd-boot.enable = true;
