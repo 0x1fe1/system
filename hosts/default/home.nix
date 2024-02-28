145f4ee40d7533c0c6f17cc148564039478cfdd9
Pangolecimal <domkuzaleza@gmail.com>
2024-02-28 22:30:24 +0300

    default: 74 current  2024-02-28 22:30:11  24.05.20240216.5863c27  6.1.77                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -44,14 +44,42 @@
         lua
         */
         ''
+          config = {}
+
           local w = require("wezterm")
-          return {
+
+          local opacity = 0.9
+          config.window_background_opacity = opacity
+
+          -- toggle function
+          wezterm.on("toggle-opacity", function(window, pane)
+            local overrides = window:get_config_overrides() or {}
+            if not overrides.window_background_opacity then
+              -- if no override is setup, override the default opacity value with 1.0
+              overrides.window_background_opacity = 1.0
+            else
+              -- if there is an override, make it nil so the opacity goes back to the default
+              overrides.window_background_opacity = nil
+            end
+            window:set_config_overrides(overrides)
+          end)
+
+          config.keys = {
+            {
+              key = "O",
+              mods = "CTRL",
+              action = wezterm.action.EmitEvent("toggle-opacity"),
+            },
+          }
+
+          config = {
             color_scheme = "Catppuccin Mocha",
             font = w.font("FiraCode Nerd Font"),
-            window_background_opacity = 0.9,
             adjust_window_size_when_changing_font_size = false,
             warn_about_missing_glyphs = false,
           }
+
+          return config
         '';
     };
 
