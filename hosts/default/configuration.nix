369f00127d5ef2ce4a5449eb60f2ac04b7e9a30e
Pangolecimal <domkuzaleza@gmail.com>
2024-03-05 17:55:45 +0300

    default: 80 current  2024-03-05 17:55:33  24.05.20240303.b8697e5  6.6.19                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -69,7 +69,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
