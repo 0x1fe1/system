e8c284215809771d814a6131411f563c5efff03b
Pangolecimal <domkuzaleza@gmail.com>
2024-03-09 21:37:46 +0300

    default: 82 current  2024-03-09 21:37:39  24.05.20240306.9df3e30  6.6.19                          *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -40,39 +40,43 @@
         lua
         */
         ''
-          config = {}
+          local wezterm = require 'wezterm';
 
-          local w = require("wezterm")
+          local config = {}
 
           config = {
-            color_scheme = "Catppuccin Mocha",
-            font = w.font("FiraCode Nerd Font"),
-            adjust_window_size_when_changing_font_size = false,
-            warn_about_missing_glyphs = false,
+              color_scheme = "Catppuccin Mocha",
+              font = wezterm.font_with_fallback {
+                  "FiraCode Nerd Font",
+                  "JetBrainsMono Nerd Font",
+                  "Consolas"
+              },
+              adjust_window_size_when_changing_font_size = false,
+              warn_about_missing_glyphs = false,
           }
 
           local opacity = 0.9
           config.window_background_opacity = opacity
 
           -- toggle function
-          wezterm.on("toggle-opacity", function(window, pane)
-            local overrides = window:get_config_overrides() or {}
-            if not overrides.window_background_opacity then
-              -- if no override is setup, override the default opacity value with 1.0
-              overrides.window_background_opacity = 1.0
-            else
-              -- if there is an override, make it nil so the opacity goes back to the default
-              overrides.window_background_opacity = nil
-            end
-            window:set_config_overrides(overrides)
+          wezterm.on("toggle-opacity", function(window, _)
+              local overrides = window:get_config_overrides() or {}
+              if not overrides.window_background_opacity then
+                  -- if no override is setup, override the default opacity value with 1.0
+                  overrides.window_background_opacity = 1.0
+              else
+                  -- if there is an override, make it nil so the opacity goes back to the default
+                  overrides.window_background_opacity = nil
+              end
+              window:set_config_overrides(overrides)
           end)
 
           config.keys = {
-            {
-              key = "O",
-              mods = "CTRL",
-              action = wezterm.action.EmitEvent("toggle-opacity"),
-            },
+              {
+                  key = "O",
+                  mods = "CTRL",
+                  action = wezterm.action.EmitEvent("toggle-opacity"),
+              },
           }
 
           return config
