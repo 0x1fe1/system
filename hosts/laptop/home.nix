91a5a6357a9dfeda266237d7335980290570d24e
Pangolecimal <domkuzaleza@gmail.com>
2024-03-10 21:58:26 +0300

    laptop: 121 current  2024-03-10 21:58:23  24.05.20240306.9df3e30  6.6.19                          *

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -35,7 +35,7 @@
       set -e
       pushd ~/system
       sudo nixos-rebuild switch --flake ~/system#laptop
-      git add . ; git commit -m \"laptop: $(nixos-rebuild list-generations --no-build-nix | grep current)\"
+      git commit -am \"laptop: $(nixos-rebuild list-generations --no-build-nix | grep current)\"
       popd
     '')
   ];
@@ -62,14 +62,14 @@
           local config = {}
 
           config = {
-              color_scheme = "Catppuccin Mocha",
-              font = wezterm.font_with_fallback {
-                  "FiraCode Nerd Font",
-                  "JetBrainsMono Nerd Font",
-                  "Consolas"
-              },
-              adjust_window_size_when_changing_font_size = false,
-              warn_about_missing_glyphs = false,
+            color_scheme = "Catppuccin Mocha",
+            font = wezterm.font_with_fallback {
+              "FiraCode Nerd Font",
+              "JetBrainsMono Nerd Font",
+              "Consolas"
+            },
+            adjust_window_size_when_changing_font_size = false,
+            warn_about_missing_glyphs = false,
           }
 
           local opacity = 0.9
@@ -77,23 +77,23 @@
 
           -- toggle function
           wezterm.on("toggle-opacity", function(window, _)
-              local overrides = window:get_config_overrides() or {}
-              if not overrides.window_background_opacity then
-                  -- if no override is setup, override the default opacity value with 1.0
-                  overrides.window_background_opacity = 1.0
-              else
-                  -- if there is an override, make it nil so the opacity goes back to the default
-                  overrides.window_background_opacity = nil
-              end
-              window:set_config_overrides(overrides)
+            local overrides = window:get_config_overrides() or {}
+            if not overrides.window_background_opacity then
+              -- if no override is setup, override the default opacity value with 1.0
+              overrides.window_background_opacity = 1.0
+            else
+              -- if there is an override, make it nil so the opacity goes back to the default
+              overrides.window_background_opacity = nil
+            end
+            window:set_config_overrides(overrides)
           end)
 
           config.keys = {
-              {
-                  key = "O",
-                  mods = "CTRL",
-                  action = wezterm.action.EmitEvent("toggle-opacity"),
-              },
+            {
+              key = "O",
+              mods = "CTRL",
+              action = wezterm.action.EmitEvent("toggle-opacity"),
+            },
           }
 
           return config
@@ -215,6 +215,7 @@
       userEmail = "domkuzaleza@gmail.com";
       extraConfig = {
         init.defaultBranch = "main";
+        core.sparseCheckout = true;
       };
     };
   };
