1b722e047361fd2b3000f426a4693d9ebe6be92f
Pangolecimal <domkuzaleza@gmail.com>
2024-03-24 22:03:42 +0300

    desktop: 106 current  2024-03-24 22:03:33  24.05.20240312.0ad13a6  6.6.21                          *

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -7,7 +7,9 @@
   home.packages = with pkgs; [
     # (jetbrains.idea-community)
     # blender
-    # jdk21
+    jdk21
+    gradle
+    gradle-completion
     kdePackages.kate
     (nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})
     brave
