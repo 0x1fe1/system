b2a262c7df0f764fb876c730c486c820bbd2569f
Pangolecimal <domkuzaleza@gmail.com>
2024-05-09 18:02:07 +0300

    desktop: 170 current  2024-05-09 18:01:57  24.05.20240312.0ad13a6  6.6.21                          *

---
+++ overlays/bazecor/default.nix
@@ -0,0 +1,9 @@
+self: super: {
+  bazecor = super.applyPatches {
+    src = super.bazecor;
+    postPatch = ''
+      rm lib/udev/rules.d/*-10-dygma.rules
+      ln -s --target-directory=lib/udev/rules.d ${./60-dygma.rules}
+    '';
+  };
+}
