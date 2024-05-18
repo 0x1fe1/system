b5be08b0033bb85a7afc7603d65a2673ddc67157
Pangolecimal <domkuzaleza@gmail.com>
2024-05-18 23:38:59 +0300

    desktop@home: 182 current  2024-05-18 20:29:52  24.05.20240507.b211b39  6.6.30                          *

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -97,24 +97,22 @@
           local FONT_ID = 1
           local FONT_ID_MAX = 4
           local FONTS = {
-            "FiraCode Nerd Font",
-            "JetBrainsMono Nerd Font",
-            "MesloLGS NF",
-            "Noto",
-            "Consolas"
+          "FiraCode Nerd Font",
+          "JetBrainsMono Nerd Font",
+          "MesloLGS Nerd Font Mono",
           }
 
           config = {
-              color_scheme = "Catppuccin Mocha (Gogh)",
-              font = w.font_with_fallback {
-                  "FiraCode Nerd Font",
-                  "JetBrainsMono Nerd Font",
-                  "Consolas"
-              },
-              adjust_window_size_when_changing_font_size = false,
-              warn_about_missing_glyphs = false,
-              -- disable_default_key_bindings = true,
-              hide_tab_bar_if_only_one_tab = true,
+            color_scheme = "Catppuccin Mocha (Gogh)",
+            font = w.font_with_fallback {
+              "FiraCode Nerd Font",
+              "JetBrainsMono Nerd Font",
+              "MesloLGS Nerd Font Mono",
+            },
+            adjust_window_size_when_changing_font_size = false,
+            warn_about_missing_glyphs = false,
+            -- disable_default_key_bindings = true,
+            hide_tab_bar_if_only_one_tab = true,
           }
 
           local opacity = 0.9
@@ -122,34 +120,32 @@
 
           -- toggle function
           w.on("toggle-opacity", function(window, _)
-              local overrides = window:get_config_overrides() or {}
-              if not overrides.window_background_opacity then
-                  -- if no override is setup, override the default opacity value with 1.0
-                  overrides.window_background_opacity = 1.0
-              else
-                  -- if there is an override, make it nil so the opacity goes back to the default
-                  overrides.window_background_opacity = nil
-              end
-              window:set_config_overrides(overrides)
+            local overrides = window:get_config_overrides() or {}
+            if not overrides.window_background_opacity then
+              overrides.window_background_opacity = 1.0
+            else
+              overrides.window_background_opacity = nil
+            end
+            window:set_config_overrides(overrides)
           end)
           w.on("font-switch", function(window, _)
-              local overrides = window:get_config_overrides() or {}
-              FONT_ID = (FONT_ID % FONT_ID_MAX) + 1
-              overrides.font = w.font(FONTS[FONT_ID])
-              window:set_config_overrides(overrides)
+            local overrides = window:get_config_overrides() or {}
+            FONT_ID = (FONT_ID % FONT_ID_MAX) + 1
+            overrides.font = w.font(FONTS[FONT_ID])
+            window:set_config_overrides(overrides)
           end)
 
           config.keys = {
-              {
-                  key = "O",
-                  mods = "CTRL",
-                  action = w.action.EmitEvent("toggle-opacity"),
-              },
-              {
-                  key = "I",
-                  mods = "CTRL",
-                  action = w.action.EmitEvent("font-switch"),
-              },
+            {
+              key = "O",
+              mods = "CTRL",
+              action = w.action.EmitEvent("toggle-opacity"),
+            },
+            {
+              key = "I",
+              mods = "CTRL",
+              action = w.action.EmitEvent("font-switch"),
+            },
           }
 
           return config
