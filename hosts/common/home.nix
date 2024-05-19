f0a7c4908d5efa64da16c2b2c8cb0612c2d1abc8
Pangolecimal <domkuzaleza@gmail.com>
2024-05-19 04:19:31 +0300

    desktop@home: 182 current  2024-05-18 20:29:52  24.05.20240507.b211b39  6.6.30                          *

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -245,6 +245,7 @@
         bash
         */
         ''
+          source ~/powerlevel10k/powerlevel10k.zsh-theme
           # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
           # Initialization code that may require console input (password prompts, [y/n]
           # confirmations, etc.) must go above this block; everything else may go below.
