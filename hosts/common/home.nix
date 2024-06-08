cdc3b103ca4168f589f3951b17e18941c2d0cedb
Pangolecimal <pangolecimal@gmail.com>
2024-06-08 14:08:24 +0300

    desktop@home: 190 current  2024-05-28 18:56:59  24.05.20240507.b211b39  6.6.30                          *

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -200,56 +200,12 @@
         }
       ];
 
-      initExtraFirst =
-        /*
-        bash
-        */
-        ''
-          source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-          # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-          # Initialization code that may require console input (password prompts, [y/n]
-          # confirmations, etc.) must go above this block; everything else may go below.
-          if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-            source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-          fi
-        '';
-
-      # initExtraBeforeCompInit =
-      #   /*
-      #   bash
-      #   */
-      #   ''
-      #     # # Set the directory we want to store zinit and plugins
-      #     # ZINIT_HOME="''${XDG_DATA_HOME:-''${HOME}/.local/share}/zinit/zinit.git"
-      #
-      #     # # Download Zinit, if it's not there yet
-      #     # if [ ! -d "$ZINIT_HOME" ]; then
-      #     #    mkdir -p "$(dirname $ZINIT_HOME)"
-      #     #    git clone https://github.com/zdharma-continuum/zinit.git --depth=1 "$ZINIT_HOME"
-      #     # fi
-      #
-      #     # # Source/Load zinit
-      #     # source "''${ZINIT_HOME}/zinit.zsh"
-      #
-      #     # # Add in Powerlevel10k
-      #     # zinit ice depth=1; zinit light romkatv/powerlevel10k
-      #     #
-      #     # # Add in zsh plugins
-      #     # zinit light zdharma-continuum/fast-syntax-highlighting
-      #     # zinit light zsh-users/zsh-completions
-      #     # zinit light zsh-users/zsh-autosuggestions
-      #     # zinit light Aloxaf/fzf-tab
-      #   '';
-
       completionInit =
         /*
         bash
         */
         ''
           # zinit cdreplay -q
-
-          # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-          [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
         '';
 
       initExtra =
@@ -341,6 +297,14 @@
       envExtra = ''
         export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix'
       '';
+    };
+
+    oh-my-posh = {
+      enable = true;
+      enableZshIntegration = true;
+      # settings = {
+      #
+      # };
     };
 
     direnv = {
