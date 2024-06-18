f189f49a7feedbd9fd5ab9e42f6100c93def7e44
0x1fe1 <pangolecimal@gmail.com>
2024-06-18 21:58:17 +0300

    desktop@system: 198 current  2024-06-18 21:58:09  24.05.20240507.b211b39  6.6.30                          *

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -42,7 +42,7 @@
     modesetting.enable = true;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
   ###
 }
