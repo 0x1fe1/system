54900112858c565e9aa025cd64de37c5a385cb2c
0x1fe1 <pangolecimal@gmail.com>
2024-06-30 22:41:50 +0300

    desktop@home: 240 current  2024-06-30 22:35:49  24.11.20240622.a71e967  6.9.6                           *

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -181,14 +181,15 @@ in
       # shellAliases = shell-aliases-common;
       functions = shell-functions-fish;
       plugins = [
-        # { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish.src; }
-        { name = "fzf"; src = pkgs.fishPlugins.fzf.src; }
+        { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish.src; }
+        # { name = "fzf"; src = pkgs.fishPlugins.fzf.src; }
       ];
       shellInitLast = /*fish*/ ''
         set -gx FZF_DEFAULT_COMMAND "fd --type f --strip-cwd-prefix"
         set -gx DIRENV_LOG_FORMAT ""
         set -U fish_greeting
-        # complete -e j
+        complete -f -c v -a '__fish_complete_path "" "Path"'
+        complete -f -c j -a '__fish_complete_path "" "Path"'
       '';
     };
 
