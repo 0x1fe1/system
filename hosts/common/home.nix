009d5958dac7b4a9fe2ca3f0f53354036be70866
0x1fe1 <pangolecimal@gmail.com>
2024-07-16 20:43:11 +0300

    desktop@home: 252 current  2024-07-16 20:40:23  24.11.20240712.7e7c39e  6.9.9                           *

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -179,6 +179,15 @@ in
 
   wayland.windowManager.sway = {
     enable = true;
+    config = {
+      modifier = "Mod4";
+      # Use kitty as default terminal
+      terminal = "wezterm";
+      # startup = [
+      #   # Launch Firefox on start
+      #   { command = "firefox"; }
+      # ];
+    };
   };
   services.clipman.enable = true;
 
