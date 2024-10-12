d9c724725daa2777f42fb8fef1f3210180151f21
0x1fe1 <pangolecimal@gmail.com>
2024-10-12 21:35:45 +0300

    desktop@system: 34 current  2024-10-12 21:35:40  24.11.20241004.bc947f5   6.6.53                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -5,14 +5,14 @@
 
 	nixpkgs.config.allowUnfree = true;
 
-	nix = let flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs; in {
+	nix = /* let flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs; in */ {
 		settings = {
 			experimental-features = [ "nix-command" "flakes" ];
-			nix-path = config.nix.nixPath;
+			# nix-path = config.nix.nixPath;
 		};
 		channel.enable = false;
-		registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-		nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+		# registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+		# nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
 	};
 
 	boot = {
