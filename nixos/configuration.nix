9fac546efeaab96494768408f2929031902f7660
Pangolecimal <domkuzaleza@gmail.com>
2024-10-22 02:28:40 +0300

    laptop@system: 213 current  2024-10-22 02:28:26  24.11.20241018.4c2fcb0  6.6.57                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -113,7 +113,7 @@
 	environment.sessionVariables = {
 		FLAKE = "/home/pango/system";
 		NIXOS_OZONE_WL = "1";
-		EDITOR = "/home/pango/neovim/result/bin/nvim";
+		EDITOR = "$(realpath ~/neovim/result/bin/nvim)";
 	};
 
 	environment.systemPackages = with pkgs; [
